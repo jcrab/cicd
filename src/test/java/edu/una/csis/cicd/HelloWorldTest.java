b4f6cd001e95ea4aa7e755cfd1f935f8d3ce4c15
Crabtree <jcrabtree@una.edu>
2024-10-28 17:40:33 -0500

    broken

--- src/test/java/edu/una/csis/cicd/HelloWorldTest.java
+++ src/test/java/edu/una/csis/cicd/HelloWorldTest.java
@@ -1,8 +1,9 @@
 package edu.una.csis.cicd;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.LinkedHashSet;
 import java.util.SequencedSet;
 
@@ -17,4 +18,9 @@ public class HelloWorldTest {
 
         assertEquals("A", alphabet.getFirst());
     }
+    
+    @Test
+	public void testMath() throws Exception {
+		assertEquals(4, 2 + 1, "bad math");
+	}
 }
